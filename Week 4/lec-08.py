30ec0d3113ac3a041b0d91451137ebbbb6036299
XaicuL <hyunjun050915@gmail.com>
2025-12-16 20:20:04 +0900

    ps code

--- Week 4/lec-08.py
+++ Week 4/lec-08.py
@@ -1,29 +1,31 @@
 #################
 ## EXAMPLE: simple Coordinate class
 #################
-class Coordinate(object):
-    """ A coordinate made up of an x and y value """
-    def __init__(self, x, y):
-        """ Sets the x and y values """
-        self.x = x
-        self.y = y
-    def __str__(self):
-        """ Returns a string representation of self """
-        return "<" + str(self.x) + "," + str(self.y) + ">"
-    def distance(self, other):
-        """ Returns the euclidean distance between two points """
-        x_diff_sq = (self.x-other.x)**2
-        y_diff_sq = (self.y-other.y)**2
-        return (x_diff_sq + y_diff_sq)**0.5
 
 
-c = Coordinate(3,4)
-origin = Coordinate(0,0)
-print(c.x, origin.x)
-print(c.distance(origin))
-print(Coordinate.distance(c, origin))
-print(origin.distance(c))
-print(c)
+# class Coordinate(object): #object is the parent class
+#     """ A coordinate made up of an x and y value """
+#     def __init__(self, x, y): #__init__ is the constructor method // #self is a reference to the object itself
+#         """ Sets the x and y values """
+#         self.x = x #self.x is an attribute of the object
+#         self.y = y #self.y is an attribute of the object
+#     def __str__(self):
+#         """ Returns a string representation of self """
+#         return "<" + str(self.x) + "," + str(self.y) + ">"
+#     def distance(self, other):
+#         """ Returns the euclidean distance between two points """
+#         x_diff_sq = (self.x-other.x)**2
+#         y_diff_sq = (self.y-other.y)**2
+#         return (x_diff_sq + y_diff_sq)**0.5
+#
+#
+# c = Coordinate(3,4)
+# origin = Coordinate(0,0)
+# print(c.x, origin.x)
+# print(c.distance(origin))
+# print(Coordinate.distance(c, origin))
+# print(origin.distance(c))
+# print(c)
 
 
 #################
@@ -31,42 +33,43 @@ print(c)
 ## Try adding more built-in operations like multiply, divide
 ### Try adding a reduce method to reduce the fraction (use gcd)
 #################
-class Fraction(object):
-    """
-    A number represented as a fraction
-    """
-    def __init__(self, num, denom):
-        """ num and denom are integers """
-        assert type(num) == int and type(denom) == int, "ints not used"
-        self.num = num
-        self.denom = denom
-    def __str__(self):
-        """ Retunrs a string representation of self """
-        return str(self.num) + "/" + str(self.denom)
-    def __add__(self, other):
-        """ Returns a new fraction representing the addition """
-        top = self.num*other.denom + self.denom*other.num
-        bott = self.denom*other.denom
-        return Fraction(top, bott)
-    def __sub__(self, other):
-        """ Returns a new fraction representing the subtraction """
-        top = self.num*other.denom - self.denom*other.num
-        bott = self.denom*other.denom
-        return Fraction(top, bott)
-    def __float__(self):
-        """ Returns a float value of the fraction """
-        return self.num/self.denom
-    def inverse(self):
-        """ Returns a new fraction representing 1/self """
-        return Fraction(self.denom, self.num)
+# class Fraction(object):
+#     """
+#     A number represented as a fraction
+#     """
+#     def __init__(self, num, denom): #parameter is num and denom
+#         """ num and denom are integers """
+#         assert type(num) == int and type(denom) == int, "ints not used" #assert is a statement that must be true
+#         self.num = num #assign num to self.num
+#         self.denom = denom #assign denom to self.denom
+#     def __str__(self): #~exist parameter
+#         """ Retunrs a string representation of self """
+#         return str(self.num) + "/" + str(self.denom)
+#     def __add__(self, other): #parameter is other
+#         """ Returns a new fraction representing the addition """
+#         top = self.num*other.denom + self.denom*other.num #def new value to top
+#         bott = self.denom*other.denom  #def new value to bott
+#         return Fraction(top, bott) #return new fraction // top and bott are parameters
+#     def __sub__(self, other): #parameter is other
+#         """ Returns a new fraction representing the subtraction """
+#         top = self.num*other.denom - self.denom*other.num #def new value to top // local value
+#         bott = self.denom*other.denom  #def new value to bott // local value
+#         return Fraction(top, bott) #return new fraction // top and bott are parameters
+#     def __float__(self):
+#         """ Returns a float value of the fraction """
+#         return self.num/self.denom #return float value of fraction
+#     def inverse(self):
+#         """ Returns a new fraction representing 1/self """
+#         return Fraction(self.denom, self.num)
+#
+# a = Fraction(1,4)
+# b = Fraction(3,4)
+# c = a + b # c is a Fraction object
+# print(c) #16/16
+# print(float(c)) #1.0
+# print(Fraction.__float__(c)) #1.0
+# print(float(b.inverse())) #1.3.(infiniteDecimal)
 
-a = Fraction(1,4)
-b = Fraction(3,4)
-c = a + b # c is a Fraction object
-print(c)
-print(float(c))
-print(Fraction.__float__(c))
-print(float(b.inverse()))
 ##c = Fraction(3.14, 2.7) # assertion error
 ##print a*b # error, did not define how to multiply two Fraction objects
 
