62f06aa899a970361f82e584de8eb90390e99b5a
XaicuL <hyunjun050915@gmail.com>
2025-12-26 01:15:05 +0900

    Update file

---
+++ Week 1/PSet-00/ps0.py
@@ -0,0 +1,11 @@
+import math
+
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+
+logx = round(math.log(x, 2), 2)
+
+x_pow_y = x ** y
+print()
+print("{} raise to {} is equal to {}".format(x, y, x_pow_y))
+print("Log of x is {}".format(logx))
