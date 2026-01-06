c4366b25c37cc88ab74a51976e1c1d1e23fbc2ff
XaicuL <hyunjun050915@gmail.com>
2026-01-07 00:05:03 +0900

    ps code

---
+++ Week 6/matplotlib/matplotlib.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+
+# plt.plot([1,2,3,4], [1,7,3,5]) #x , y
+# plt.show()
+
+# plt.figure(1)
+# plt.plot([1,2,3,4], [1,2,3,4])
+# plt.figure(2)
+# plt.plot([1,4,2,3], [5,6,7,8])
+# plt.savefig('Figure-Addie')
+# plt.figure(1)
+#
+# # plt.show()
+#
+#
+# plt.plot([5,6,10,3])
+# plt.savefig('Figure-Jane')
+#
+# plt.show()
+
+# prin = 10000
+# rate = 0.05
+# years = 20
+# val = []
+#
+# for i in range(years + 1):
+#     val.append(prin)
+#     prin += prin * rate
+#
+# plt.plot(val)
+# plt.title('5% Growth, Compounded Annually')
+# plt.xlabel('Years')
+# plt.ylabel('val')
+# plt.savefig('Figure-Growth')
+#
+# plt.show()
+
+#
+# x = [3,5,23,34]
+# y = [2,43,24,23]
+#
+# plt.xlabel("x")
+# plt.ylabel("y")
+#
+# # plt.plot([1,2],[1,2])
+# # plt.plot([1,2],[2,1])
+# plt.plot(x,y) #Can use variable
+#
+#
+# plt.show()
