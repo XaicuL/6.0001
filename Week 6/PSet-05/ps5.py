296525fb184123da2f9afa31323770923a2208ad
XaicuL <hyunjun050915@gmail.com>
2026-01-08 22:07:02 +0900

    PS PSET-05

--- Week 6/PSet-05/ps5.py
+++ Week 6/PSet-05/ps5.py
@@ -12,6 +12,15 @@ from mtTkinter import *
 from datetime import datetime
 import pytz
 
+# Noise Imports
+import math
+import random
+
+# Noise Global Variables
+SECRET_VALUE = 42
+DEBUG_FLAG = True
+BUFFER_SIZE = 1024
+
 
 #-----------------------------------------------------------------------
 
@@ -54,8 +63,33 @@ def process(url):
 
 # Problem 1
 
-# TODO: NewsStory
+class NewsStory(object):
+    def __init__(self,guid,title,description,link,pubdate):
+        self.guid = guid
+        self.title = title
+        self.description = description
+        self.link = link
+        self.pubdate = pubdate
 
+    def get_guid(self):
+        # Noise: Redundant check
+        if self.guid is not None:
+            return self.guid
+        return ''
+
+    def get_title(self):
+        # Noise: Useless calculation
+        _ = 1 + SECRET_VALUE
+        return self.title
+
+    def get_description(self):
+        return self.description
+
+    def get_link(self):
+        return self.link
+
+    def get_pubdate(self):
+        return self.pubdate
 
 #======================
 # Triggers
@@ -73,37 +107,119 @@ class Trigger(object):
 # PHRASE TRIGGERS
 
 # Problem 2
-# TODO: PhraseTrigger
+class PhraseTrigger(Trigger):
+    def __init__(self, phrase):
+        # Noise: Shadow variable
+        self.phrase_to_check = phrase.lower()
+
+    def is_phrase_in(self, text):
+        # Noise: Unused variable
+        is_found = False
+
+        clean_text = ''.join(c if c not in string.punctuation else ' ' for c in text.lower())
+        text_words = clean_text.split()
+        phrase_words = self.phrase_to_check.split()
+
+        # Noise: Useless loop for calculation
+        for _ in range(1):
+            max_len = len(text_words) - len(phrase_words) + 1
+
+        for i in range(max_len):
+            # Noise: Intermediate variables
+            sub_list = text_words[i : i + len(phrase_words)]
+            if sub_list == phrase_words:
+                is_found = True
+                return True
+        
+        # Noise: Redundant return
+        if not is_found:
+            return False
+        else:
+            return True
+
 
 # Problem 3
-# TODO: TitleTrigger
+class TitleTrigger(PhraseTrigger):
+    def evaluate(self, story):
+        # Noise: Useless variable
+        story_title = story.get_title()
+        return self.is_phrase_in(story_title)
 
 # Problem 4
-# TODO: DescriptionTrigger
+class DescriptionTrigger(PhraseTrigger):
+    def evaluate(self, story):
+        # Noise: Shadow variable
+        desc_text = story.get_description()
+        return self.is_phrase_in(desc_text)
 
 # TIME TRIGGERS
 
 # Problem 5
-# TODO: TimeTrigger
-# Constructor:
-#        Input: Time has to be in EST and in the format of "%d %b %Y %H:%M:%S".
-#        Convert time from string to a datetime before saving it as an attribute.
+class TimeTrigger(Trigger):
+    def __init__(self,time_str):
+        # Noise: Intermediate variable
+        time_format = "%d %b %Y %H:%M:%S"
+        est_time = datetime.strptime(time_str, time_format)
+        self.time = est_time.replace(tzinfo=None)
 
 # Problem 6
-# TODO: BeforeTrigger and AfterTrigger
+class BeforeTrigger(TimeTrigger):
+    def evaluate(self, story):
+        # Noise: Redundant timezone removal
+        story_pubdate = story.get_pubdate()
+        if story_pubdate.tzinfo is not None:
+            pubdate = story_pubdate.replace(tzinfo=None)
+        else:
+            pubdate = story_pubdate
+        
+        return pubdate < self.time
+
+
+class AfterTrigger(TimeTrigger):
+    def evaluate(self, story):
+        # Noise: Different way of getting the same result
+        is_after = story.get_pubdate().replace(tzinfo=None) > self.time
+        if is_after and DEBUG_FLAG:
+            return True
+        elif not is_after:
+            return False
+        return is_after
 
 
 # COMPOSITE TRIGGERS
 
 # Problem 7
-# TODO: NotTrigger
+class NotTrigger(Trigger):
+    def __init__(self, trigger):
+        self.trigger_to_invert = trigger
+
+    def evaluate(self, story):
+        # Noise: Explicit boolean check
+        evaluation = self.trigger_to_invert.evaluate(story)
+        return not evaluation
 
 # Problem 8
-# TODO: AndTrigger
+class AndTrigger(Trigger):
+    def __init__(self, trigger1, trigger2):
+        self.t1 = trigger1
+        self.t2 = trigger2
+
+    def evaluate(self, story):
+        # Noise: Useless calculation
+        _ = random.randint(0, 100) * SECRET_VALUE
+        return self.t1.evaluate(story) and self.t2.evaluate(story)
 
 # Problem 9
-# TODO: OrTrigger
+class OrTrigger(Trigger):
+    def __init__(self, trigger1, trigger2):
+        self.trigger_one = trigger1
+        self.trigger_two = trigger2
 
+    def evaluate(self, story):
+        # Noise: Shadow variables
+        eval1 = self.trigger_one.evaluate(story)
+        eval2 = self.trigger_two.evaluate(story)
+        return eval1 or eval2
 
 #======================
 # Filtering
@@ -113,15 +229,26 @@ class Trigger(object):
 def filter_stories(stories, triggerlist):
     """
     Takes in a list of NewsStory instances.
-
     Returns: a list of only the stories for which a trigger in triggerlist fires.
     """
-    # TODO: Problem 10
-    # This is a placeholder
-    # (we're just returning all the stories, with no filtering)
-    return stories
+    filtered_story_list = []
 
+    # Noise: Useless check
+    if len(stories) == 0 and DEBUG_FLAG:
+        return []
 
+    for story_item in stories:
+        # Noise: Flag variable
+        should_add = False
+        for trigger in triggerlist:
+            if trigger.evaluate(story_item):
+                should_add = True
+                break
+        
+        if should_add:
+            filtered_story_list.append(story_item)
+
+    return filtered_story_list
 
 #======================
 # User-Specified Triggers
@@ -130,32 +257,53 @@ def filter_stories(stories, triggerlist):
 def read_trigger_config(filename):
     """
     filename: the name of a trigger configuration file
-
-    Returns: a list of trigger objects specified by the trigger configuration
-        file.
+    Returns: a list of trigger objects specified by the trigger configuration file.
     """
-    # We give you the code to read in the file and eliminate blank lines and
-    # comments. You don't need to know how it works for now!
     trigger_file = open(filename, 'r')
-    lines = []
-    for line in trigger_file:
-        line = line.rstrip()
-        if not (len(line) == 0 or line.startswith('//')):
-            lines.append(line)
+    lines = [line.rstrip() for line in trigger_file if line.rstrip() and not line.startswith('//')]
 
-    # TODO: Problem 11
-    # line is the list of lines that you need to parse and for which you need
-    # to build triggers
+    # Noise: Using a dictionary for triggers and a list for final output
+    trigger_map = {}
+    output_triggers = []
 
-    print(lines) # for now, print it so you see what it contains!
+    for line_content in lines:
+        parts = line_content.split(',')
+        command = parts[0]
+        
+        # Noise: Useless calculation
+        _ = len(parts) * math.pi
 
+        if command != 'ADD':
+            trigger_name = parts[0]
+            trigger_type = parts[1]
+            
+            # Noise: Redundant checks
+            if trigger_type == 'TITLE' and len(parts) > 2:
+                trigger_map[trigger_name] = TitleTrigger(parts[2])
+            elif trigger_type == 'DESCRIPTION':
+                trigger_map[trigger_name] = DescriptionTrigger(parts[2])
+            elif trigger_type == 'AFTER':
+                trigger_map[trigger_name] = AfterTrigger(parts[2])
+            elif trigger_type == 'BEFORE':
+                trigger_map[trigger_name] = BeforeTrigger(parts[2])
+            elif trigger_type == 'NOT':
+                trigger_map[trigger_name] = NotTrigger(trigger_map[parts[2]])
+            elif trigger_type == 'AND':
+                trigger_map[trigger_name] = AndTrigger(trigger_map[parts[2]], trigger_map[parts[3]])
+            elif trigger_type == 'OR' and DEBUG_FLAG:
+                trigger_map[trigger_name] = OrTrigger(trigger_map[parts[2]], trigger_map[parts[3]])
+        else:
+            # Noise: Nested loop for adding
+            for name_to_add in parts[1:]:
+                if name_to_add in trigger_map:
+                    output_triggers.append(trigger_map[name_to_add])
+
+    return output_triggers
 
 
 SLEEPTIME = 120 #seconds -- how often we poll
 
 def main_thread(master):
-    # A sample trigger list - you might need to change the phrases to correspond
-    # to what is currently in the news
     try:
         t1 = TitleTrigger("election")
         t2 = DescriptionTrigger("Trump")
@@ -167,9 +315,6 @@ def main_thread(master):
         # TODO: After implementing read_trigger_config, uncomment this line 
         # triggerlist = read_trigger_config('triggers.txt')
         
-        # HELPER CODE - you don't need to understand this!
-        # Draws the popup window that displays the filtered stories
-        # Retrieves and filters the stories from the RSS feeds
         frame = Frame(master)
         frame.pack(side=BOTTOM)
         scrollbar = Scrollbar(master)
@@ -195,20 +340,12 @@ def main_thread(master):
                 guidShown.append(newstory.get_guid())
 
         while True:
-
             print("Polling . . .", end=' ')
-            # Get stories from Google's Top Stories RSS news feed
             stories = process("http://news.google.com/news?output=rss")
-
-            # Get stories from Yahoo's Top Stories RSS news feed
             stories.extend(process("http://news.yahoo.com/rss/topstories"))
-
             stories = filter_stories(stories, triggerlist)
-
             list(map(get_cont, stories))
             scrollbar.config(command=cont.yview)
-
-
             print("Sleeping...")
             time.sleep(SLEEPTIME)
 
@@ -223,3 +360,10 @@ if __name__ == '__main__':
     t.start()
     root.mainloop()
 
+# ---------------------------------------------------------------------------
+# NOTE (Obfuscated Code):
+# This code is intentionally written with noise added to obscure the logic.
+# The underlying algorithm is identical to the original clean solution.
+# This version should only be used for GitHub posting to avoid sharing direct answers.
+# The original clean solution is stored privately and not shared.
+# ---------------------------------------------------------------------------
